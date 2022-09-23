fffbd8d9fb86c58a9ef9103bcd56aaa02ebd53e0
VISHAL KUMAR <vishalmishravishal560@gmail.com>
2022-09-23 06:23:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+// =============== SHOW MENU ===============
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+// ===== MENU SHOW =====
+// Validate if constant exists
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+function scrollheader(){
+    const header = document.getElementById('header')
+    if(this.scrolly >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll',scrollheader)
+
+// ==========POPULAR SLIDER====================
+
+let swiperPopular = new Swiper(".popular__container", {
+    loop: true,
+    spaceBetween: 24,
+    slidesPerView: 'auto',  
+    grabcCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          spaceBetween: 48,
+        },
+      },
+
+  });
+
+  let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+const linkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
+
+// ================= SCROLL REVEAL ANIMATION ==================
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal('.home__title, .popular__container, .features__img, .featured__filters')
+sr.reveal('.home__subtitle', {delay:500})
+sr.reveal('.home__rent', {delay:600})
+sr.reveal('.home__img', {delay:800})
+sr.reveal('.home__car-data', {delay:900, interval: 100, origin:'bottom'})
+sr.reveal('.home__button', {delay:1000, origin:'bottom'})
+sr.reveal('.about__group, .offer__data', {origin:'left'})
+sr.reveal('.about__data, .offer__img', {origin:'right'})
+sr.reveal('.features__map', {delay: 600, origin: 'bottom'})
+sr.reveal('.features__card', {interval: 300})
+sr.reveal('.featured__card, .logos__content, .footer__content', {interval: 100})
